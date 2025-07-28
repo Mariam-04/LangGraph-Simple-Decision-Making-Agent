b33eb3f74606499f6b8f478f4df653ba7862dad9
Mariam-04 <mararif2004@gmail.com>
2025-07-28 16:45:52 +0500

    Initial commit: InternGPT RAG assistant

---
+++ prompt_templates.py
@@ -0,0 +1,16 @@
+# prompt_templates.py
+
+qa_prompt = """
+You are InternGPT, a helpful assistant that answers internship-related queries.
+
+Use the following context to answer the question as accurately as possible.
+If the answer is not in the context, say "I’m sorry, I couldn’t find relevant information."
+
+Context:
+{context}
+
+Question:
+{question}
+
+Answer:
+"""
